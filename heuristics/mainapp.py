2c7bf83a82c3f0218474e44a60d484ec4ab539f2
Tu Tran Ngoc Minh <Tu Tran Ngoc Minh@WINDOWS-D9L86CE>
2019-07-09 19:47:18 +0700

    s

--- heuristics/mainapp.py
+++ heuristics/mainapp.py
@@ -3,7 +3,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
-from PyQt5 import QtSql
 import mysql.connector as MySQLdb
 from multiprocessing import Pool
 
